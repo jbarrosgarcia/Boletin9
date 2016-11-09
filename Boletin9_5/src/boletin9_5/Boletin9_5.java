2cb5cadbacd811c13663c6724b349035e896efa4
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-11-09 10:05:56 +0100

    Commit final de esta clase

--- Boletin9_5/src/boletin9_5/Boletin9_5.java
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -5,6 +5,8 @@
  */
 package boletin9_5;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Jose Barros
@@ -16,6 +18,11 @@ public class Boletin9_5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cadenas cad=new Cadenas();
+        System.out.println(cad.cadenaFib(Integer.parseInt(JOptionPane.showInputDialog("Introduzca la longitud de la serie de Fibonacci"))));
+        System.out.println(cad.cadenaPar(Integer.parseInt(JOptionPane.showInputDialog("Introduzca la cantidad de numeros pares que quiere visualizar"))));
+        System.out.println(cad.cadenaRestas(Integer.parseInt(JOptionPane.showInputDialog("Introduzca la cantidad de numeros"))));
+        
     }
     
 }
